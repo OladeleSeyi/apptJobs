8ef0b980fcf5891baf2186175b69d7cd6d695639
kosi uhuegbu <kosiuhuegbu@gmail.com>
2022-03-10 15:51:26 +0100

    added the whatsapp get started link as a button

--- src/pages/Home/components/Hero.js
+++ src/pages/Home/components/Hero.js
@@ -7,6 +7,8 @@ import { css } from "styled-components/macro";
 import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-1.svg";
 import DesignIllustration from "images/design-illustration-2.svg";
 
+import { PrimaryButton as PrimaryButtonBase } from 'components/misc/Buttons.js';
+
 const Container = tw.div`relative`;
 const TwoColumn = tw.div`flex flex-col lg:flex-row lg:items-center max-w-screen-xl mx-auto py-20 md:py-24`;
 const LeftColumn = tw.div`relative lg:w-5/12 text-center max-w-lg mx-auto lg:max-w-none lg:text-left`;
@@ -17,42 +19,57 @@ const Paragraph = tw.p`my-5 lg:my-8 text-base xl:text-lg`;
 
 const IllustrationContainer = tw.div`flex justify-center lg:justify-end items-center`;
 
+const PrimaryButton = tw.button`px-8 py-3 font-bold rounded bg-primary-500 text-gray-100 hocus:bg-primary-700 hocus:text-gray-200 focus:shadow-outline focus:outline-none transition duration-300`;
+
 // Random Decorator Blobs (shapes that you see in background)
 const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
-  ${tw`pointer-events-none opacity-5 absolute left-0 bottom-0 h-64 w-64 transform -translate-x-2/3 -z-10`}
+	${tw`pointer-events-none opacity-5 absolute left-0 bottom-0 h-64 w-64 transform -translate-x-2/3 -z-10`}
 `;
 
 export default ({
-  roundedHeaderButton,
-  heading = (
-    <>
-      We <span tw="text-primary-500">Hunt</span> you
-      <span tw="text-primary-500">Apply</span>
-    </>
-  ),
-  paragraph = "Automate your Job Search On WhatsApp Get Started",
-  img = DesignIllustration,
+	roundedHeaderButton,
+	heading = (
+		<>
+			We <span tw='text-primary-500'>Hunt</span> you
+			<span tw='text-primary-500'>Apply</span>
+		</>
+	),
+
+	primaryButtonText = 'Get Started',
+	primaryButtonUrl = 'https://wa.me/message/H6LUUKPUCUEBN1',
+	buttonRounded = true,
+	Target = '_blank',
+
+	paragraph = 'Automate your Job Search On WhatsApp',
+	img = DesignIllustration,
 }) => {
-  return (
-    <>
-      <Container>
-        <TwoColumn>
-          <LeftColumn>
-            <Heading>{heading}</Heading>
-            <Paragraph>{paragraph}</Paragraph>
-          </LeftColumn>
-          <RightColumn>
-            <IllustrationContainer>
-              <img
-                tw="min-w-0 w-full max-w-lg xl:max-w-3xl"
-                src={img}
-                alt="Design Illustration"
-              />
-            </IllustrationContainer>
-          </RightColumn>
-        </TwoColumn>
-        <DecoratorBlob1 />
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Container>
+				<TwoColumn>
+					<LeftColumn>
+						<Heading>{heading}</Heading>
+						<Paragraph>{paragraph}</Paragraph>
+						<PrimaryButton
+							buttonRounded={buttonRounded}
+							as='a'
+							href={primaryButtonUrl}
+							target={Target}>
+							{primaryButtonText}
+						</PrimaryButton>
+					</LeftColumn>
+					<RightColumn>
+						<IllustrationContainer>
+							<img
+								tw='min-w-0 w-full max-w-lg xl:max-w-3xl'
+								src={img}
+								alt='Design Illustration'
+							/>
+						</IllustrationContainer>
+					</RightColumn>
+				</TwoColumn>
+				<DecoratorBlob1 />
+			</Container>
+		</>
+	);
 };
