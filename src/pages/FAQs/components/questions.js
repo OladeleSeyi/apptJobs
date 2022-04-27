6b9494f20dea14f8774b2357dfbd376c665230cd
oladeleseyi <toothy61@gmail.com>
2022-04-27 22:44:52 +0100

    use question comp

--- src/pages/FAQs/components/questions.js
+++ src/pages/FAQs/components/questions.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-
+import React from "react";
 
 import styled from "styled-components";
 import tw from "twin.macro";
@@ -8,8 +7,6 @@ import { css } from "styled-components/macro";
 import { SectionHeading } from "components/misc/Headings.js";
 
 import { ReactComponent as SvgDecoratorBlob3 } from "images/svg-decorator-blob-3.svg";
-
-import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
 
 import whatsapp from "images/whatsapp-faq.svg";
 import questionMark from "images/question-mark.svg";
@@ -21,6 +18,7 @@ import start from "images/start.svg";
 import money from "images/money.svg";
 
 import infinity from "images/infinity.svg";
+import Question from "./question";
 
 const Container = tw.div`relative`;
 
@@ -28,36 +26,6 @@ const ThreeColumnContainer = styled.div`
   ${tw`flex flex-col items-center md:items-stretch md:flex-row flex-wrap md:justify-center max-w-screen-xl mx-auto py-20 md:py-24`}
 `;
 const Heading = tw(SectionHeading)`w-full`;
-
-const Column = styled.div`
-  ${tw`md:w-1/2 lg:w-1/3 px-6`}
-`;
-
-const Card = styled.div`
-  ${tw`flex flex-col mx-auto max-w-xs items-center px-6 py-10 border-2 border-dashed border-primary-500 rounded-lg mt-12`}
-  .imageContainer {
-    ${tw`border-2 border-primary-500 text-center rounded-full p-6 flex-shrink-0 relative`}
-    img {
-      ${tw`w-8 h-8`}
-    }
-  }
-
-  .textContainer {
-    ${tw`mt-6 text-center`}
-  }
-
-  .title {
-    ${tw`mt-2 font-bold text-xl leading-none text-primary-500`}
-  }
-
-  .description {
-    ${tw`mt-3 font-semibold text-secondary-100 text-sm leading-loose`}
-  }
-`;
-
-const PrimaryButton = styled(PrimaryButtonBase)`
-  ${tw`mt-4 text-sm inline-block mx-auto md:mx-0 rounded-full`}
-`;
 
 const DecoratorBlob = styled(SvgDecoratorBlob3)`
   ${tw`pointer-events-none absolute right-0 bottom-0 w-64 opacity-25 transform translate-x-32 translate-y-48 `}
@@ -104,22 +72,18 @@ const howMuchFaq = [
 ];
 
 const cvFaq = [
-	<div>
-		{
-			<p>
-				Yes, they are by default necessary for job applications to give
-				an edge. <br /> Create a winning CV{' '}
-				<a href='https://flowcv.io/'>here</a> <br />
-  
-				Contact our Customer Support for a winning Cover Letter
-				<a href='https://wa.me/message/H6LUUKPUCUEBN1'>here</a> <br />
-
-				Cover Letter is best sent as the body of the mail, not as an
-				attached document, did you know that?
-			</p>
-		}
-	</div>,
-
+  <div>
+    {
+      <p>
+        Yes, they are by default necessary for job applications to give an edge.{" "}
+        <br /> Create a winning CV <a href="https://flowcv.io/">here</a> <br />
+        Contact our Customer Support for a winning Cover Letter
+        <a href="https://wa.me/message/H6LUUKPUCUEBN1">here</a> <br />
+        Cover Letter is best sent as the body of the mail, not as an attached
+        document, did you know that?
+      </p>
+    }
+  </div>,
 ];
 
 const startFaq = [
@@ -166,122 +130,41 @@ export default () => {
    *  If a key for a particular card is not provided, a default value is used
    */
 
-  const [readMore, setReadMore] = useState(false);
-  const linkName = readMore ? "Read Less << " : "Read More >> ";
-
-
   const cards = [
     {
       imageSrc: questionMark,
       title: "What is Appt Jobs?",
-      description: (
-        <div>
-          <PrimaryButton
-            onClick={() => {
-              setReadMore(!readMore);
-            }}
-          >
-            <h2>{linkName}</h2>
-          </PrimaryButton>
-          {readMore && whatIsApptFaq}
-        </div>
-      ),
+      description: whatIsApptFaq,
     },
     {
       imageSrc: resume,
       title: "Do I need a CV, Cover Letter?",
-      description: (
-        <div>
-          <PrimaryButton
-            onClick={() => {
-              setReadMore(!readMore);
-            }}
-          >
-            <h2>{linkName}</h2>
-          </PrimaryButton>
-          {readMore && cvFaq}
-        </div>
-      ),
+      description: cvFaq,
     },
     {
       imageSrc: whatsapp,
       title: "Why Whatsapp?",
-      description: (
-        <div>
-          <PrimaryButton
-            onClick={() => {
-              setReadMore(!readMore);
-            }}
-          >
-            <h2>{linkName}</h2>
-          </PrimaryButton>
-          {readMore && whyWhatsappFaq}
-        </div>
-      ),
+      description: whyWhatsappFaq,
     },
     {
       imageSrc: start,
       title: "How do I start?",
-      description: (
-        <div>
-          <PrimaryButton
-            onClick={() => {
-              setReadMore(!readMore);
-            }}
-          >
-            <h2>{linkName}</h2>
-          </PrimaryButton>
-          {readMore && startFaq}
-        </div>
-      ),
+      description: startFaq,
     },
     {
       imageSrc: infinity,
       title: "How many jobs vacancies will I receive?",
-      description: (
-        <div>
-          <PrimaryButton
-            onClick={() => {
-              setReadMore(!readMore);
-            }}
-          >
-            <h2>{linkName}</h2>
-          </PrimaryButton>
-          {readMore && howManyFaq}
-        </div>
-      ),
+      description: howManyFaq,
     },
     {
       imageSrc: money,
       title: "Do I have to pay for a Job?",
-      description: (
-        <div>
-          <PrimaryButton
-            onClick={() => {
-              setReadMore(!readMore);
-            }}
-          >
-            <h2>{linkName}</h2>
-          </PrimaryButton>
-          {readMore && howMuchFaq}
-        </div>
-      ),
+      description: howMuchFaq,
     },
     {
       imageSrc: naira,
       title: "Do I have to pay once I secure a job?",
-      description: (
-        <div>
-          <PrimaryButton
-            onClick={() => {
-              setReadMore(!readMore);
-            }}
-          >
-            <h2>{linkName}</h2>
-          </PrimaryButton>
-          {readMore && payFaq}
-        </div>
-      ),
+      description: payFaq,
     },
   ];
 
@@ -290,17 +173,7 @@ export default () => {
       <ThreeColumnContainer>
         <Heading>Frequently Asked Questions</Heading>
         {cards.map((card, i) => (
-          <Column key={i}>
-            <Card>
-              <span className="imageContainer">
-                <img src={card.imageSrc} alt="" />
-              </span>
-              <span className="textContainer">
-                <span className="title">{card.title}</span>
-                <p className="description">{card.description}</p>
-              </span>
-            </Card>
-          </Column>
+          <Question card={card} />
         ))}
       </ThreeColumnContainer>
       <DecoratorBlob />
